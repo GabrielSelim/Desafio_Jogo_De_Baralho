4c492544d5f276dfe67cfe01a3419116ef4bfa44
Gabriel Sanz <gabriel.sanz@infortechms.com.br>
2025-03-19 13:56:14 -0400

    alterado para 5000

--- Desafio_Jogo_De_Baralho/Program.cs
+++ Desafio_Jogo_De_Baralho/Program.cs
@@ -3,6 +3,12 @@ using Desafio_Jogo_De_Baralho.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configurar Kestrel para escutar na porta 5000
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5000);
+});
+
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -18,7 +24,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Desafio Jogo De Baralho API V1");
+        c.RoutePrefix = string.Empty; // Para acessar o Swagger na raiz
+    });
 }
 
 app.UseHttpsRedirection();
